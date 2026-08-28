2ba153ff692f498af389a3b08d6105943c4d41a2
Shivantra <dev@shivantra.com>
2026-08-28 12:54:21 +0530

    Updates

--- assets/Eyebrow-C8vuDdA9.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-B0jULZz8.js";function r({children:t}){return e.jsx("span",{className:"text-faint text-[12.5px] font-semibold uppercase tracking-[0.08em]",children:t})}export{r as E};
