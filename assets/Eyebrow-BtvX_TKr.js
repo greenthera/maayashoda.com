e615b42612cef8fc31ea45aaedd452bef9064a0b
Shivantra <dev@shivantra.com>
2026-08-28 11:40:43 +0530

    Updates

---
+++ assets/Eyebrow-BtvX_TKr.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-ab8UegTH.js";function r({children:t}){return e.jsx("span",{className:"text-faint text-[12.5px] font-semibold uppercase tracking-[0.08em]",children:t})}export{r as E};
