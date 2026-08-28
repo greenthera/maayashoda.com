5b77abacf2a65640461bf3d0d7274c37e4d9eef0
Shivantra <dev@shivantra.com>
2026-08-28 11:40:20 +0530

    chore: add path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
   ssgOptions: {
     includedRoutes(paths) {
       return paths.filter((p) => !p.includes("*"));
